b04e9b3a643df8a8df167811722ee44c5a64cb44
Feber Cruz <code.lebers@gmail.com>
2024-03-18 07:16:32 -0600

    reportes

--- routers/moduloInventario/reporte_router.py
+++ routers/moduloInventario/reporte_router.py
@@ -11,10 +11,10 @@ from functions.dependencies import get_current_user
 
 router = APIRouter()
 
-@router.get("/reportes/cajas", response_model=List[Caja])
+@router.get("/reportes/cajas", tags=["Reportes"], response_model=List[Caja])
 def reporte_cajas(fecha_inicio: date, fecha_fin: date, user: str = Depends(get_current_user)):
     return reporte_controller.get_reporte_cajas(fecha_inicio, fecha_fin)
 
-@router.get("/reportes/carpetas", response_model=List[Carpeta])
+@router.get("/reportes/carpetas", tags=["Reportes"], response_model=List[Carpeta])
 def reporte_carpetas(fecha_inicio: date, fecha_fin: date, user: str = Depends(get_current_user)):
     return reporte_controller.get_reporte_carpetas(fecha_inicio, fecha_fin)
